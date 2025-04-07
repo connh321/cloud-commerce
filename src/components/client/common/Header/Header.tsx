fbbf8ac4df0b0f2017e0371154d2bfe39cb5e768
Connor <Connor@MacBookPro.lan>
2025-04-07 17:43:09 -0400

    Added logo

--- src/components/client/common/Header/Header.tsx
+++ src/components/client/common/Header/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 import { HiOutlineSearch } from 'react-icons/hi';
 import { IoArrowBackOutline } from 'react-icons/io5';
@@ -7,13 +7,24 @@ import Navigation from '@/components/client/common/Navigation/Navigation';
 import { useGlobalContext } from '@/context/Global/GlobalContext';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const Header = () => {
   const { setIsSearchActive, setHidePrimaryColor } = useGlobalContext();
   const router = useRouter();
   const pathname = usePathname();
+  const [windowWidth, setWindowWidth] = useState(0);
 
-  useEffect(() => {}, [pathname]);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  useEffect(() => {}, []);
 
   const focus = (): void => {
     if (window.innerWidth <= 768) {
@@ -41,7 +52,20 @@ const Header = () => {
         <div
           className={`${styles.logo} ${pathname === '/' || pathname === '/products' || pathname === '/home' ? styles.hideLogoMobile : ''}`}>
           <Link href="/" className={styles.link}>
-            Logo
+            <Image
+              src={
+                windowWidth <= 768
+                  ? '/images/cc_black_logo.png'
+                  : '/images/cc_logo.png'
+              }
+              alt={'logo'}
+              className={styles.image}
+              style={{ objectFit: 'contain' }}
+              width={115}
+              height={45}
+              priority
+              unoptimized
+            />
           </Link>
         </div>
         <div
